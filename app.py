f87944cf197d0ead21ee0742c4b97798a4e22300
binhquytoc-lab <binhquytoc@gmail.com>
2026-08-16 15:08:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.image("logo.jpg")
+st.image("logo1.jpg")
 import pandas as pd
 from datetime import datetime
 st.set_page_config(page_title="Order Nhà Hàng", layout="wide")
